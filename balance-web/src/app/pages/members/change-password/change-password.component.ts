0a702f80b9cfe0dd3d8185e483eaa13c28fcc5f1
Min Lwin <lwin.zawmin@gmail.com>
2024-10-02 12:49:15 +0630

    Update Ledger Management View

--- balance-web/src/app/pages/members/change-password/change-password.component.ts
+++ balance-web/src/app/pages/members/change-password/change-password.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { WidgetsModule } from '../../../widgets/widgets.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-change-password',
   standalone: true,
-  imports: [WidgetsModule],
+  imports: [WidgetsModule, ReactiveFormsModule],
   templateUrl: './change-password.component.html',
   styles: ``
 })
 export class ChangePasswordComponent {
 
+  form:FormGroup
+
+  constructor(builder:FormBuilder) {
+    this.form = builder.group({
+      oldPass : ['', [Validators.required, Validators.minLength(6)]],
+      newPass : ['', [Validators.required, Validators.minLength(6)]],
+    })
+  }
 }
