433b84c3a7fe188e22d4fbdfac2344a16c4a8dc6
Min Lwin <lwin.zawmin@gmail.com>
2024-10-02 11:03:22 +0630

    Update Balance Report View

--- balance-web/src/app/widgets/widgets.module.ts
+++ balance-web/src/app/widgets/widgets.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormGroupComponent } from './form-group/form-group.component';
 import { PageContentComponent } from './page-content/page-content.component';
+import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
     FormGroupComponent,
-    PageContentComponent
+    PageContentComponent,
+    PaginationComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     FormGroupComponent,
-    PageContentComponent
+    PageContentComponent,
+    PaginationComponent
   ]
 })
 export class WidgetsModule { }
