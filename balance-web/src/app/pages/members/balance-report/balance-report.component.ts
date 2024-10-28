323235738b01e61c49c094a650a654727dd24d43
Min Lwin <lwin.zawmin@gmail.com>
2024-10-29 02:28:41 +0630

    update for report

--- balance-web/src/app/pages/members/balance-report/balance-report.component.ts
+++ balance-web/src/app/pages/members/balance-report/balance-report.component.ts
@@ -1,13 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { WidgetsModule } from '../../../widgets/widgets.module';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { BalanceReportService } from '../../../services/api/balance-report.service';
+import { PagerComponent } from '../../../widgets/pagination/pagination.component';
+import { PageInfo } from '../../../services/commons';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-balance-report',
   standalone: true,
-  imports: [WidgetsModule],
+  imports: [WidgetsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './balance-report.component.html',
   styles: ``
 })
-export class BalanceReportComponent {
+export class BalanceReportComponent implements PagerComponent{
 
+  form:FormGroup
+
+  pageInfo = signal<PageInfo | undefined>(undefined)
+  contents = computed(() => this.pageInfo()?.contents || [])
+
+  constructor(builder:FormBuilder,
+    private service:BalanceReportService) {
+    this.form = builder.group({
+      from: '',
+      to: '',
+      ledger: '',
+      page: 0,
+      size: 10
+    })
+  }
+
+  onLinkChange(page: number): void {
+    this.form.patchValue({page: page})
+    this.search()
+  }
+
+  onSizeChange(size: number): void {
+    this.form.patchValue({size: size, page: 0})
+    this.search()
+  }
+
+  search() {
+    this.service.search(this.form.value)
+  }
 }
--- balance-web/src/app/pages/members/change-password/change-password.component.ts
+++ balance-web/src/app/pages/members/change-password/change-password.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { WidgetsModule } from '../../../widgets/widgets.module';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ChangePasswordService } from '../../../services/api/change-password.service';
+import { LoginUserState } from '../../../services/security/login-user.state';
 
 @Component({
   selector: 'app-change-password',
@@ -13,10 +15,21 @@ export class ChangePasswordComponent {
 
   form:FormGroup
 
-  constructor(builder:FormBuilder) {
+  constructor(builder:FormBuilder,
+    private loginUser:LoginUserState,
+    private service:ChangePasswordService) {
     this.form = builder.group({
+      username: loginUser.user()?.loginId,
       oldPass : ['', [Validators.required, Validators.minLength(6)]],
       newPass : ['', [Validators.required, Validators.minLength(6)]],
     })
+  }
+
+  changePass() {
+    if(this.form.valid) {
+      this.service.changePassword(this.form.value).subscribe(result => {
+        this.loginUser.setUser(result)
+      })
+    }
   }
 }
